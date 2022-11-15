04e83d516fd9314393ceff9057800ada845c9a3d
janis-1986 <108686569+janis-1986@users.noreply.github.com>
2022-11-15 11:12:48 +0200

    Add files via upload

---
+++ BOMB.java
@@ -0,0 +1,15 @@
+public class BOMB {
+    public static void main(String[] args) {
+
+
+        for (int x = 5; x > 0; x--) {
+            System.out.println("Time" + x);
+
+            if (x == 1)
+                System.out.println("BOOM!");
+
+
+        }
+
+    }
+}
